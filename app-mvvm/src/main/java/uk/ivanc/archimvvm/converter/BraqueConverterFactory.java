12c01f20f768f1abe22f76b8e9f7e8d6c0d6cc9e
Mike Solomon <mike@mikesolomon.org>
2016-10-05 13:21:47 +0300

    refactors to use generic adaptor

--- app-mvvm/src/main/java/uk/ivanc/archimvvm/converter/BraqueConverterFactory.java
+++ app-mvvm/src/main/java/uk/ivanc/archimvvm/converter/BraqueConverterFactory.java
@@ -1,12 +1,21 @@
 package uk.ivanc.archimvvm.converter;
 
+import android.util.Log;
+
+import com.google.inject.util.Types;
+
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Type;
+import java.util.List;
 
+import braque.braqued.StringProvisioner;
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Converter;
 import retrofit2.Retrofit;
+import uk.ivanc.archimvvm.api.braqued.BrowseGithubsShowGithubThing;
+
+import static uk.ivanc.archimvvm.converter.BraqueResponseBodyConverter.BraqueProperty;
 
 /**
  * Created by mikesolomon on 05/10/16.
@@ -22,12 +31,25 @@ public final class BraqueConverterFactory extends Converter.Factory {
     @Override
     public Converter<ResponseBody, ?> responseBodyConverter(Type type, Annotation[] annotations,
                                                             Retrofit retrofit) {
-        return new BraqueResponseBodyConverter();
+        if (type.toString().equals(Types.newParameterizedType(List.class, BrowseGithubsShowGithubThing.class).toString())) {
+            Log.d("BraqueConverterFactory","converter worked");
+            return new BraqueResponseBodyConverter(BrowseGithubsShowGithubThing.class, "id",
+                    new BraqueResponseBodyConverter.BraqueProperty[]{
+                            new BraqueProperty("stargazers_count", StringProvisioner.propStars()),
+                            new BraqueProperty("name", StringProvisioner.propName()),
+                            new BraqueProperty("forks_count", StringProvisioner.propForks()),
+                            new BraqueProperty("watchers_count", StringProvisioner.propWatchers()),
+                            new BraqueProperty("language", StringProvisioner.propLanguage()),
+                            new BraqueProperty("owner.name", StringProvisioner.propOwner()),
+                            new BraqueProperty("owner.avatar_url", StringProvisioner.propAvatarURL())
+                    });
+        }
+        return null;
     }
 
     @Override
     public Converter<?, RequestBody> requestBodyConverter(Type type, Annotation[] parameterAnnotations,
                                                           Annotation[] methodAnnotations, Retrofit retrofit) {
-        return new BraqueRequestBodyConverter();
+        return null;
     }
 }
--- app-mvvm/src/main/java/uk/ivanc/archimvvm/converter/BraqueResponseBodyConverter.java
+++ app-mvvm/src/main/java/uk/ivanc/archimvvm/converter/BraqueResponseBodyConverter.java
@@ -17,9 +17,18 @@ import braque.braqued.StringProvisioner;
 import okhttp3.ResponseBody;
 import retrofit2.Converter;
 import uk.ivanc.archimvvm.Utils;
-import uk.ivanc.archimvvm.api.braqued.BrowseGithubsShowGithubThing;
 
 public class BraqueResponseBodyConverter implements Converter<ResponseBody, Collection<? extends RESTOperation>> {
+
+    private final Class<? extends RESTOperation> deserializingTo;
+    private final String idProp;
+    private final BraqueProperty[] properties;
+
+    BraqueResponseBodyConverter(Class<? extends RESTOperation> deserializingTo, String idProp, BraqueProperty[] properties) {
+        this.deserializingTo = deserializingTo;
+        this.idProp = idProp;
+        this.properties = properties;
+    }
 
     static class BraqueProperty {
         final String receivedName;
@@ -30,53 +39,44 @@ public class BraqueResponseBodyConverter implements Converter<ResponseBody, Coll
         }
     }
 
-    static private final BraqueProperty[] repoProperties = new BraqueProperty[]{
-            new BraqueProperty("stargazers_count", StringProvisioner.propStars()),
-            new BraqueProperty("name", StringProvisioner.propName()),
-            new BraqueProperty("forks_count", StringProvisioner.propForks()),
-            new BraqueProperty("watchers_count", StringProvisioner.propWatchers()),
-            new BraqueProperty("language", StringProvisioner.propLanguage())
-    };
-    static private final BraqueProperty[] ownerProperties = new BraqueProperty[] {
-            new BraqueProperty("name", StringProvisioner.propOwner()),
-            new BraqueProperty("avatar_url", StringProvisioner.propAvatarURL())
-    };
-
     @Override public Collection<? extends RESTOperation> convert(ResponseBody value) throws IOException {
         try {
-            Log.d("BraqueConverter", "working");
             JSONObject jsonObject = new JSONObject(value.string());
             JSONArray items = jsonObject.getJSONArray("items");
             Map<String, Object> serialized = new HashMap<>();
 
             for (int i = 0; i < items.length(); i++) {
                 JSONObject item = items.getJSONObject(i);
-                if (!item.has("id")) {
+                if (!item.has(idProp)) {
+                    Log.e("BraqueConverter", "item lacks "+idProp);
                     continue;
                 }
-                String id = Integer.toString(item.getInt("id"));
+                String id = Integer.toString(item.getInt(idProp));
                 serialized.put(Utils.toBraquePath(StringProvisioner.pathBrowseGithubs(), id, "id"), id);
-                for (BraqueProperty property : repoProperties) {
-                    if (item.has(property.receivedName)) {
-                        serialized.put(Utils.toBraquePath(StringProvisioner.pathBrowseGithubs(), id, property.usedName),
-                                item.get(property.receivedName));
-                    }
-                }
-                if (item.has("owner")) {
-                    JSONObject owner = item.getJSONObject("owner");
-                    for (BraqueProperty property : ownerProperties) {
-                        if (owner.has(property.receivedName)) {
-                            serialized.put(Utils.toBraquePath(StringProvisioner.pathBrowseGithubs(), id, property.usedName),
-                                    owner.get(property.receivedName));
+                for (BraqueProperty property : properties) {
+                    JSONObject thisItem = item;
+                    String[] splitPath = property.receivedName.split("\\.");
+                    int j = 0;
+                    for (; j < splitPath.length - 1; j++) {
+                        if (thisItem.has(splitPath[j])) {
+                            thisItem = thisItem.getJSONObject(splitPath[j]);
+                        } else {
+                            break;
                         }
+                    }
+                    if (j < (splitPath.length - 1)) {
+                        continue;
+                    }
+                    if (thisItem.has(splitPath[j])) {
+                        serialized.put(Utils.toBraquePath(StringProvisioner.pathBrowseGithubs(), id, property.usedName),
+                                thisItem.get(splitPath[j]));
                     }
                 }
             }//for (Map.Entry<String,Object> entry : serialized.entrySet()){Log.d("BraqueConverter", entry.getKey()+" "+entry.getValue());}
-            Collection<? extends RESTOperation> coll = Deserializer.deserialize(serialized, BrowseGithubsShowGithubThing.class);
-            Log.d("BraqueConverter", "size="+coll.size());
+            Collection<? extends RESTOperation> coll = Deserializer.deserialize(serialized, deserializingTo);
             return coll;
         } catch (JSONException e) {
-            Log.d("BraqueConverter", e.toString());
+            Log.e("BraqueConverter", e.toString());
             return null;
         } finally {
             value.close();
